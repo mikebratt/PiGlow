80dc6e862a3f66de2b124ba0355ed8c2d1ff0132
Mike Bratt <git.bratt@otherinbox.co.uk>
2013-10-31 14:08:42 +0000

    Added more CPU usage levels

--- Examples/cpu.py
+++ Examples/cpu.py
@@ -17,26 +17,28 @@ while True:
     cpu = psutil.cpu_percent()
     piglow.all(0)
 
-    if cpu < 5:
-        piglow.white(20)
+    if cpu < 1:
+        piglow.white(0)
+    elif cpu < 10:
+        piglow.white(5)
     elif cpu < 20:
-        piglow.white(20)
+        piglow.white(10)
         piglow.blue(20)
     elif cpu < 40:
-        piglow.white(20)
-        piglow.blue(20)
-        piglow.green(20)
+        piglow.white(10)
+        piglow.blue(35)
+        piglow.green(35)
     elif cpu < 60:
         piglow.white(20)
-        piglow.blue(20)
-        piglow.green(20)
-        piglow.yellow(20)
+        piglow.blue(40)
+        piglow.green(40)
+        piglow.yellow(40)
     elif cpu < 80:
         piglow.white(20)
-        piglow.blue(20)
-        piglow.green(20)
-        piglow.yellow(20)
-        piglow.orange(20)
+        piglow.blue(40)
+        piglow.green(40)
+        piglow.yellow(40)
+        piglow.orange(100)
     else:
         piglow.all(20)
     sleep(0.2)
